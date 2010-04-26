a9495567c5d33bb94ed991bf7755ceef34ed5117
alexgirao <alexgirao@alexgirao-ti.(none)>
2010-04-26 19:26:52 -0300

    wip, port.java async read isn't detecting pipe close

--- interoperability/ports/java/port.java
+++ interoperability/ports/java/port.java
@@ -12,12 +12,25 @@ class port {
 	byte[] buffer = new byte[0xffff];
 
 	for (;;) {
-	    int n = read(System.in, buffer);
-	    hexdump.dump(buffer, 0, System.err, 0, n);
+	    int n = async_read(System.in, buffer);
+	    assert n >= 0;
+	    if (n > 0) {
+		hexdump.dump(buffer, 0, System.err, 0, n);
+
+		System.out.write(0);
+		System.out.write(0);
+		System.out.write(0);
+		System.out.write(1);
+		System.out.write(123);
+		System.out.flush();      // don't use buffered streams, flush immediately
+	    } else {
+		// sleep here, so we don't stress cpu
+		Thread.sleep(10); // miliseconds
+	    }
 	}
     }
 
-    private static int read(final InputStream src, byte[] buffer) throws java.io.IOException {
+    private static int async_read(final InputStream src, byte[] buffer) throws java.io.IOException {
 	int len_available = src.available();
 
 	assert len_available >= 0;
