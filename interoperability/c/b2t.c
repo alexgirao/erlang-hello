9c19fcc3e2b1966f85df1d02e6bad9c54219f3dd
alexgirao <alexgirao@alexgirao-ti.(none)>
2010-06-02 20:45:44 -0300

    checkpoint 20100602T234544140UTC

--- interoperability/c/b2t.c
+++ interoperability/c/b2t.c
@@ -20,53 +20,33 @@
 
 /* processing function, return amount of successfully processed data
  */
-static int doit(const char *buf, int len, hcns(bool) eof)
+static struct eterm *doit2(const char *buf, int len, int *index, int depth, struct eterm *parent)
 {
-	int version;
-	int index;
-	ei_term e[1];
-	char *str;
+	ei_term *e;
 	int r;
-
-	assert(eof == 1);  /* eof is expected since the buffer is
-			    * sufficiently large and in erlang's
-			    * distribution protocol we now the buffer
-			    * size in advance */
-
-	index = 0;
-
-	assert(ei_decode_version(buf, &index, &version) == 0);
-	assert(version == 131 /* 130 in erlang 4.2 */);
-	
+	struct eterm *h = NULL;
 	int safeloop = 100;
+	/* int skipindex; */
+	int n = parent ? parent->t->arity : -1;
 
-	while (index < len && safeloop) {
-		int prior = index;
+	while (*index < len && n-- && safeloop) {
+		int prior = *index;
 		int ttype, tlen;
+
+		h = eterm_new0(h);
+		e = h->t;
 
 		ttype = -1;
 		tlen = -1;
 
-		assert(ei_get_type(buf, &index, &ttype, &tlen) == 0);
+		assert(ei_get_type(buf, index, &ttype, &tlen) == 0);
+		e->ei_type = ttype;
 		if (ttype == 70) {
-			hcns(u4) u4u4[2];
-			int endiancheck = 0xdeadbeef;
-
 			/* unsupported type by erl_interface
 			 */
-			index += sizeof(e->ei_type);
-
 			switch (ttype) {
-			case 70: /* public static final int newFloatTag = 70: from otp_src_R13B04/lib/jinterface/java_src/com/ericsson/otp/erlang/OtpExternal.java */
-
-				assert(*((unsigned char*)&endiancheck) == 0xef);  /* little endian only so far */
-
-				u4u4[1] = HC_GET_BE4(buf + index);
-				u4u4[0] = HC_GET_BE4(buf + index + 4);
-
-				e->value.d_val = (double) *((double*)u4u4);
-				index += sizeof(double);
-
+			case 70: /* EEE 754 */
+				decode_double_type70(buf, index, &e->value.d_val);
 				fprintf(stderr, "TYPE 70: %f\n", e->value.d_val);
 				break;
 			default:
@@ -77,65 +57,105 @@ static int doit(const char *buf, int len, hcns(bool) eof)
 			e->arity = -1;
 			e->size = -1;
 
-			r = ei_decode_ei_term(buf, &index, e);
+			r = ei_decode_ei_term(buf, index, e);
 			if (r == -1) {
-				HC_FATAL("failed to decode term, index: %i (type %i)", index, e->ei_type);
+				HC_FATAL("failed to decode term, *index: %i (type %i)", *index, e->ei_type);
 			}
 			assert(r == 0 || r == 1);
-			assert(r == 0 || prior != index);  /* when r == 1, buffer was consumed */
+			assert(r == 0 || prior != *index);  /* when r == 1, buffer was consumed */
 
 			switch (e->ei_type) {
 			case ERL_SMALL_INTEGER_EXT:
-				if (HC_ISGRAPH(e->value.i_val)) {
-					fprintf(stderr, "ERL_SMALL_INTEGER_EXT: %li [%c] [a: %i]\n",
-						e->value.i_val, (unsigned char)e->value.i_val, e->arity);
-				} else {
-					fprintf(stderr, "ERL_SMALL_INTEGER_EXT: %li\n", e->value.i_val);
-				}
+			case ERL_ATOM_EXT:
+			case ERL_INTEGER_EXT:
+			case ERL_FLOAT_EXT:
+				/* simple types
+				 */
+				assert(r == 1);
 				break;
 			case ERL_STRING_EXT:
-				/* int ei_decode_string(const char *buf, int *index, char *p)
+				hcns(s_alloc)(h->str, e->size + 1);  /* as manual says */
+				assert(ei_decode_string(buf, index, h->str->s) == 0);
+				break;
+			case ERL_SMALL_TUPLE_EXT:
+			case ERL_LARGE_TUPLE_EXT:
+			case ERL_LIST_EXT:
+				/* compound types
 				 */
-				HC_ALLOC(str, e->size + 1);
-				assert(ei_decode_string(buf, &index, str) == 0);
-				str[e->size] = '\0';
-				fprintf(stderr, "ERL_STRING_EXT: [%s] [a: %i]\n", str, e->arity);
-				HC_FREE(str);
+				assert(r == 1);
+				h->children = doit2(buf, len, index, depth + 1, h);
 				break;
-			case ERL_INTEGER_EXT:
-				fprintf(stderr, "ERL_INTEGER_EXT: %li\n", (long)e->value.i_val);
-				break;
-			case ERL_FLOAT_EXT:
-				fprintf(stderr, "ERL_FLOAT_EXT: %f\n", e->value.d_val);
-				break;
-			case ERL_ATOM_EXT:
 			case ERL_REFERENCE_EXT:
 			case ERL_NEW_REFERENCE_EXT:
 			case ERL_PORT_EXT:
 			case ERL_PID_EXT:
-			case ERL_SMALL_TUPLE_EXT:
-			case ERL_LARGE_TUPLE_EXT:
-			case ERL_NIL_EXT:
 			case ERL_BINARY_EXT:
 			case ERL_SMALL_BIG_EXT:
 			case ERL_LARGE_BIG_EXT:
 			case ERL_PASS_THROUGH:
-			case ERL_LIST_EXT:
 			case ERL_NEW_CACHE:
 			case ERL_CACHED_ATOM:
 				fprintf(stderr, "unimplemented, skipping: i=%i (was %i), r=%i, t=%i (%c), a=%i, s=%i\n",
-					index, prior, r, e->ei_type, e->ei_type, e->arity, e->size);
-				ei_skip_term(buf, &index);
+					*index, prior, r, e->ei_type, e->ei_type, e->arity, e->size);
+				*index = prior; /* rewind term to skip */
+				//ei_skip_term(buf, index);
+				assert(skip_term(buf, index) == 0);
+				fprintf(stderr, "index after skip: %i\n", *index);
+				break;
+			case ERL_NIL_EXT:
+				assert(ei_decode_list_header(buf, index, NULL) == 0);
+				assert(*index == len || depth > 0);
 				break;
 			default:
 				fprintf(stderr, "unknown type: i=%i (was %i), r=%i, t=%i (%c), a=%i, s=%i\n",
-					index, prior, r, e->ei_type, e->ei_type, e->arity, e->size);
+					*index, prior, r, e->ei_type, e->ei_type, e->arity, e->size);
 				continue;
 			}
 		}
 
 		safeloop--;
 	}
+
+	return h;
+}
+
+static void eterm_show(int level, struct eterm *h)
+{
+	struct eterm_iter i[1];
+
+	eterm_forward(h, i);
+	while ((h = eterm_next(i))) {
+		printf("%i: %p: %i\n", level, h, h->t->ei_type);
+		if (h->children) {
+			eterm_show(level + 1, h->children);
+		}
+	}
+	eterm_end(i);
+}
+
+static int doit(const char *buf, int len, hcns(bool) eof)
+{
+	struct eterm *h = NULL;
+	int index = 0;
+	int version;
+
+	assert(eof == 1);  /* eof is expected since the buffer is
+			    * sufficiently large and in erlang's
+			    * distribution protocol we now the buffer
+			    * size in advance */
+
+
+	assert(ei_decode_version(buf, &index, &version) == 0);
+	assert(version == 131 /* 130 in erlang 4.2 */);
+
+	h = doit2(buf, len, &index, 0, h);
+
+	eterm_show(0, h);
+
+	/* clean up
+	 */
+
+	eterm_free0(h);
 
 	fprintf(stderr, "processed %i bytes\n", len);
 	return len;
@@ -154,10 +174,12 @@ int main(int argc, char **argv)
 	fprintf(stderr, "total bytes read: %i\n", hcns(readfd)(fd, buf, bufsz, doit));
 	close(fd);
 
+#if 0
 	fprintf(stderr, "reading %s\n", "t2b.c.bin");
 	fd = open("t2b.c.bin", O_RDONLY);
 	fprintf(stderr, "total bytes read: %i\n", hcns(readfd)(fd, buf, bufsz, doit));
 	close(fd);
+#endif
 
 	HC_FREE(buf);
 
--- interoperability/c/common-inc.c
+++ interoperability/c/common-inc.c
@@ -142,3 +142,113 @@ void encode_empty_list(HC_ST_S *x)
 	hcns(s_alloc)(x, i);
 	ei_encode_empty_list(x->s, &x->len);
 }
+
+int decode_double_type70(const char *buf, int *index, double *p)
+{
+	const char *s = buf + *index;
+	const char *s0 = s;
+	int endiancheck = 0xdeadbeef;
+	hcns(u4) u4u4[2];
+
+	if (*s++ != 70) return -1;
+	
+	//if (sscanf(s, "%lf", &f) != 1) return -1;
+
+	assert(*((unsigned char*)&endiancheck) == 0xef);  /* little endian only so far */
+	
+	u4u4[1] = HC_GET_BE4(s);
+	u4u4[0] = HC_GET_BE4(s + 4);
+	
+	s += sizeof(double);
+	if (p) *p = *((double*)u4u4);
+	*index += s-s0;
+	return 0;
+}
+
+int skip_term(const char* buf, int *index)
+{
+	int i, n, ty;
+
+	/* ASSERT(ep != NULL); */
+
+	ei_get_type_internal(buf, index, &ty, &n);
+	switch (ty) {
+	case ERL_ATOM_EXT:
+		/* FIXME: what if some weird locale is in use? */
+		if (ei_decode_atom(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_PID_EXT:
+		if (ei_decode_pid(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_PORT_EXT:
+		if (ei_decode_port(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_NEW_REFERENCE_EXT:
+	case ERL_REFERENCE_EXT:
+		if (ei_decode_ref(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_NIL_EXT:
+		if (ei_decode_list_header(buf, index, &n) < 0) return -1;
+		break;
+	case ERL_LIST_EXT:
+		if (ei_decode_list_header(buf, index, &n) < 0) return -1;
+		for (i = 0; i < n; ++i) {
+			if (skip_term(buf, index) < 0) {
+				return -1;
+			}
+		}
+		if (ei_get_type_internal(buf, index, &ty, &n) < 0) {
+			return -1;
+		}
+		if (ty != ERL_NIL_EXT) {
+			if (skip_term(buf, index) < 0) return -1;
+		} else if (ei_decode_list_header(buf, index, &n) < 0) {
+			return -1;
+		}
+		break;
+	case ERL_STRING_EXT:
+		if (ei_decode_string(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_SMALL_TUPLE_EXT:
+	case ERL_LARGE_TUPLE_EXT:
+		if (ei_decode_tuple_header(buf, index, &n) < 0) return -1;
+		for (i = 0; i < n; ++i)
+			skip_term(buf, index);
+		break;
+	case ERL_BINARY_EXT:
+		if (ei_decode_binary(buf, index, NULL, NULL) < 0)
+			return -1;
+		break;
+	case ERL_SMALL_INTEGER_EXT:
+	case ERL_INTEGER_EXT:
+		if (ei_decode_long(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_SMALL_BIG_EXT:
+	case ERL_LARGE_BIG_EXT:
+		if (ei_decode_big(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_FLOAT_EXT:
+		if (ei_decode_double(buf, index, NULL) < 0) return -1;
+		break;
+	case 70: /* public static final int newFloatTag = 70: from otp_src_R13B04/lib/jinterface/java_src/com/ericsson/otp/erlang/OtpExternal.java */
+		if (decode_double_type70(buf, index, NULL) < 0) return -1;
+		break;
+	case ERL_FUN_EXT:
+	case ERL_NEW_FUN_EXT:
+		if (ei_decode_fun(buf, index, NULL) < 0) return -1;
+		break;
+	default:
+		return -1;
+	}
+
+	return 0;
+}
+
+/* highlevel erlang terms
+ */
+
+HC_DECL_PRIVATE_I(eterm,
+		ei_term t[1]; /* type in t->ei_type */
+		struct eterm *children;  /* for lists and tuples, length in t->arity */
+		HC_ST_S str[1]; /* ERL_STRING_EXT / aka byte list */
+);
